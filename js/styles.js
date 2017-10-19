3d46e5430b2926cd4e204fa5fbce988e5c1ae212
Lew Davidson and Lena Ha <lew@>
2017-10-19 13:49:14 -0700

    add conditional statements to javascript, edit html

--- js/styles.js
+++ js/styles.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
   $("form#celeb-form").submit(function(event) {
     var age = parseInt($("input#age").val());
-    var gender = $("option#gender").val();
-    var personality =  $("input:radio[name=personality]:checked").val();
-    var name =  $("input#name").val();
+    var gender = $("select#gender").val();
+    var personality = $("input:radio[name=personality]:checked").val();
+    var name = $("input#name").val();
 
-    if (gender === "#female" && age <= 30 && personality === "#goofy" || personality === "#laidback") {
+    if (gender === "female" && age >= 30) {
       $("#mcjagger").show()
     }
-
+    if (gender === "female" && age <= 30) {
+      $("#marilyn-manson").show()
+    }
+    if (gender === "male" && age <= 100) {
+      $("#beyonce").show()
+    }
+  
     event.preventDefault();
   });
 });
